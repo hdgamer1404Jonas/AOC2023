4e33ead33e08366403252bfec058507aa08f36a3
hdgamer1404Jonas <hdgamer1404Jonas@gmail.com>
2023-12-02 22:59:27 +0100

    Day 1 complete

--- day1/index-2.js
+++ day1/index-2.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 
-const input = fs.readFileSync("test.txt", "utf-8");
+const input = fs.readFileSync("input.txt", "utf-8");
 const array = input.split("\n");
 
 console.log(array);
@@ -9,70 +9,46 @@ console.log(array);
 
 
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let numChars = {
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9"
-}
 
 let calibrations = [];
 
 for (let i = 0; i < array.length; i++) {
-    let line = array[i];
-	let lineArr = array[i].split("");
+
+    // replace "one" with "o1e", "two" with "t2o", etc.
+    for (let j = 0; j < array[i].length; j++) {
+        array[i] = array[i].replace("one", "o1e");
+        array[i] = array[i].replace("two", "t2o");
+        array[i] = array[i].replace("three", "th3ee");
+        array[i] = array[i].replace("four", "f4ur");
+        array[i] = array[i].replace("five", "f5ve");
+        array[i] = array[i].replace("six", "s6x");
+        array[i] = array[i].replace("seven", "se7en");
+        array[i] = array[i].replace("eight", "ei8ht");
+        array[i] = array[i].replace("nine", "n9ne");
+    }
+
+    console.log(array[i]);
+
+	let line = array[i].split("");
 	
 	if (array[i] === "") continue;
+
+	console.log(i + ". " + array[i]  + ": " + line);
 	
 
 	let firstNum;
 	let secondNum;
 
-	for (let j = 0; j < lineArr.length; j++) {
-		let char = lineArr[j];
-
-        for (let key in numChars) {
-            let keyLength = key.length;
-
-            // get the amount of chars after the current char (including the current char)
-            let charsAfter = line.slice(j, j + keyLength);
-            console.log(charsAfter);
-            // check if the chars include the key
-            if (charsAfter.includes(key)) {
-                firstNum = numChars[key];
-                break;
-            }
-        }
-
-
+	for (let j = 0; j < line.length; j++) {
+		let char = line[j];
 		if (numbers.includes(char)) {
 			firstNum = char;
 			break;
 		}
 	}
 
-	for (let j = lineArr.length; j >= 0; j--) {
-		let char = lineArr[j];
-        
-        for (let key in numChars) {
-            let keyLength = key.length;
-
-            // get the amount of chars after the current char (including the current char)
-            let charsAfter = line.slice(j, j + keyLength);
-            console.log(charsAfter);
-            // check if the chars include the key
-            if (charsAfter.includes(key)) {
-                secondNum = numChars[key];
-                break;
-            }
-        }
-
-
+	for (let j = line.length; j >= 0; j--) {
+		let char = line[j];
 		if (numbers.includes(char)) {
 			secondNum = char;
 			break;
@@ -91,6 +67,7 @@ let sum = 0;
 for (let i = 0; i < calibrations.length; i++) {
 	let parsedInt = parseInt(calibrations[i]);
 	sum += parsedInt;
+	console.log(calibrations[i] + ": " + parsedInt);
 } 
 
 console.log(sum);
